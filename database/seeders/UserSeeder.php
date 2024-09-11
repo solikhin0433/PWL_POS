878eeb06582e9d40a3d923067dc0c5c25468f364
Solikhin <solikhinkhin645@gmail.com>
2024-09-12 00:22:58 +0700

    Pertemuan 3_Praktikum 6

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -36,9 +36,9 @@ class UserSeeder extends Seeder
                 'nama' => 'Staff/Kasir',
                 'password' => Hash::make('12345'),
             ],
+
         ];
 
         DB::table('m_user')->insert($data);
-
     }
 }
