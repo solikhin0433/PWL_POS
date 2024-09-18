0c8d06ebf13d518820afdd3222959be11429e63c
Solikhin <solikhinkhin645@gmail.com>
2024-09-18 09:02:54 +0700

    Pertemuan 4_Praktikum 2.1  Retrieving  single Mdl

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,20 +10,27 @@ use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     public function index()
-    {  
+    {
         // tambah data user dengan Eloquent Model
-        $data = [
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 2',
-            'password' => Hash::make('12345'),
+        // $data = [
+        //     'level_id' => 2,
+        //     'username' => 'manager_tiga',
+        //     'nama' => 'Manager 2',
+        //     'password' => Hash::make('12345'),
 
-        ];
+        // ];
 
-        UserModel::create($data);
-
-        // coba akses model UserModel
-        $user = UserModel::all(); // ambil semua data dari tabel m_user
+        // UserModel::create($data);
+        // // coba akses model UserModel
+        // $user = UserModel::all(); // ambil semua data dari tabel m_user
+        // return view('user', ['data' => $user]);
+        // $user = UserModel::find(1);
+        // $user = UserModel :: where('level_id', 1)->first();
+        // return view('user', ['data' => $user]);
+        // $user = UserModel :: firstWhere('level_id', 1);
+        $user = UserModel::findOr(20, ['username', 'nama'], function () {
+            abort(404);
+        });
         return view('user', ['data' => $user]);
     }
 }
--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -5,10 +5,26 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class usermodel extends Model
+// class usermodel extends Model
+// {
+//     use HasFactory;
+
+//     protected $table = 'm_user'; //mendefinisikan nama tabel yang digunakan oleh model ini
+//     protected $primaryKey = 'user_id'; //mendefinisikan Primary key dari table yang digunakan
+// }
+class UserModel extends Model
 {
     use HasFactory;
 
-    protected $table = 'm_user'; //mendefinisikan nama tabel yang digunakan oleh model ini
-    protected $primaryKey = 'user_id'; //mendefinisikan Primary key dari table yang digunakan
+    protected $table = 'm_user';
+    protected $primaryKey = 'user_id';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    //protected $fillable = ['level_id','username','nama','password',];
+
+    protected $fillable = ['level_id','username','nama'];
 }
