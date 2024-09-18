5584acbaa03668e3212600b02ab62d39f19315d1
Solikhin <solikhinkhin645@gmail.com>
2024-09-18 13:56:27 +0700

    Pertemuan 4_Praktikum 2.6  CRUD

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -1,27 +1,26 @@
 <!DOCTYPE html>
 <html>
-     <head>
-     <title>Data User</title>
-     </head>
-     <body>
+    <body>
           <h1>Data User</h1>
+          <a href="{{url('/user/tambah')}}"> + Tambah User</a>
           <table border="1" cellpadding="2" cellspacing="0">
     <tr>
         {{-- <td>Jumlah Pengguna</td> --}}
         <td>ID</td>
         <td>Username</td>
         <td>Nama</td>
-        <th>ID Level Pengguna</th>
+        <td>ID Level Pengguna</td>
+        <td>Aksi</td>
     </tr>
-    {{-- @foreach ($data as $d) --}}
-    <tr>
+    @foreach($data as $d) 
         {{-- <td>{{ $data}}</td> --}}
-        <td>{{ $data->id }}</td>
-        <td>{{ $data->username }}</td>
-        <td>{{ $data->nama }}</td>
-        <td>{{ $data->level_id }}</td>
+        <td>{{ $d->user_id }}</td>
+        <td>{{ $d->username }}</td>
+        <td>{{ $d->nama }}</td>
+        <td>{{ $d->level_id }}</td>
+        <td><a href="{{ url('/user/ubah/' . $d->user_id) }}">Ubah</a>|<a href="{{url('/user/hapus/'. $d->user_id) }}">Hapus</a></td>
     </tr>
-    {{-- @endforeach --}}
+    @endforeach
           </table>
      </body>
 </html>
---
+++ resources/views/user_ubah.blade.php
@@ -0,0 +1,28 @@
+<body>
+     <h1>Form Ubah Data User</h1>
+ 
+     <a href="{{url('/user')}}">Kembali</a>
+     <br><br>
+ 
+     <form method="post" action="{{url('/user/ubah_simpan/'.$data->user_id)}}">
+         {{ csrf_field()}}       
+         {{ method_field('PUT')}}
+ 
+         <label for="username">Username</label>
+         <input type="text" name="username" placeholder="Masukkan Username" value="{{ $data->username }}">
+         <br>
+ 
+         <label for="nama">Nama</label>
+         <input type="text" name="nama" placeholder="Masukkan Nama" value="{{ $data->nama }}">
+         <br>
+ 
+         <label for="password">Password</label>
+         <input type="password" name="password" placeholder="Masukkan Password" value="{{ $data->password }}">
+         <br>
+ 
+         <label for="level ID">Level ID</label>
+         <input type="number" name="level_id" placeholder="Masukkan ID Level" value="{{ $data->level_id }}">
+         <br><br>
+         <input type="submit" class="btn btn-success" value="Ubah">
+     </form>
+ </body>
