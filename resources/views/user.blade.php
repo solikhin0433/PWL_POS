fb94b2f072ac78b26b2191179fe96d53a1a2cea9
Solikhin <solikhinkhin645@gmail.com>
2024-09-18 09:29:08 +0700

    Pertemuan 4_Praktikum 2.3  Retreiving Aggregrates

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -7,17 +7,17 @@
           <h1>Data User</h1>
           <table border="1" cellpadding="2" cellspacing="0">
     <tr>
-        <td>ID</td>
-        <td>Username</td>
+        <td>Jumlah Pengguna</td>
+        {{-- <td>Username</td>
         <td>Nama</td>
-        <th>ID Level Pengguna</th>
+        <th>ID Level Pengguna</th> --}}
     </tr>
     {{-- @foreach ($data as $d) --}}
     <tr>
-        <td>{{ $data->user_id }}</td>
-        <td>{{ $data->username }}</td>
+        <td>{{ $data}}</td>
+        {{-- <td>{{ $data->username }}</td>
         <td>{{ $data->nama }}</td>
-        <td>{{ $data->level_id }}</td>
+        <td>{{ $data->level_id }}</td> --}}
     </tr>
     {{-- @endforeach --}}
           </table>
