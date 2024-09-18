199f428ea44308e98d8962a1517bec126944454e
Solikhin <solikhinkhin645@gmail.com>
2024-09-18 08:31:09 +0700

    Pertemuan 4_Praktikum 1 fillable

--- app/Http/Controllers/LevelController.php
+++ app/Http/Controllers/LevelController.php
@@ -7,9 +7,9 @@ use Illuminate\Support\Facades\DB;
 class LevelController extends Controller
 { 
     public function index(){
-   // DB::insert('insert into m_level( level_kode, level_nama, created_at) 
-       //  values(?,?,?)',['CUS','Pelanggan', now()]);
-        // return 'insert data baru berhasil';
+       DB::insert('insert into m_level( level_kode, level_nama, created_at) 
+       values(?,?,?)',['CUS','Pelanggan', now()]);
+        return 'insert data baru berhasil';
         
         // $row = DB::update('update m_level set level_nama = ? where level_kode = ?', ['Customer','CUS']);
         //  return 'Update data berhasil, Jumlah data yang diupdate: '. $row.'baris';
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -13,17 +13,17 @@ class UserController extends Controller
     {  
         // tambah data user dengan Eloquent Model
         $data = [
-            
-            'username' => 'customer-1',
-            'nama' => 'Pelanggan',
+            'level_id' => 2,
+            'username' => 'manager_tiga',
+            'nama' => 'Manager 2',
             'password' => Hash::make('12345'),
-            'level_id' => 4
+
         ];
 
-        UserModel::insert($data); // tambahkan data ke tabel m_user
+        UserModel::create($data);
 
         // coba akses model UserModel
-        //$user = UserModel::all(); // ambil semua data dari tabel m_user
-        //return view('user', ['data' => $user]);
+        $user = UserModel::all(); // ambil semua data dari tabel m_user
+        return view('user', ['data' => $user]);
     }
 }
