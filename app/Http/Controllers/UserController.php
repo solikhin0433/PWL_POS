87d913d9af07322f5448a124991d55bdec763d0e
Solikhin <solikhinkhin645@gmail.com>
2024-09-18 09:15:29 +0700

    Pertemuan 4_Praktikum 2.2 Not Found Exceptions

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -28,9 +28,11 @@ class UserController extends Controller
         // $user = UserModel :: where('level_id', 1)->first();
         // return view('user', ['data' => $user]);
         // $user = UserModel :: firstWhere('level_id', 1);
-        $user = UserModel::findOr(20, ['username', 'nama'], function () {
-            abort(404);
-        });
+        // $user = UserModel::findOr(20, ['username', 'nama'], function () {
+        //     abort(404);
+        // });
+        // $user = UserModel:: findOrFail(1);
+        $user = UserModel::where('username', 'manager9')->firstOrFail();
         return view('user', ['data' => $user]);
     }
 }
