438f47124951054d9164332f2b7fae6cfd2e2db0
Solikhin <solikhinkhin645@gmail.com>
2024-09-18 15:12:46 +0700

    Pertemuan 4_Praktikum 2.7 Relationships

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -91,7 +91,11 @@ class UserController extends Controller
         // $user->wasChanged(['username', 'level_id']); // true
         // $user->wasChanged('nama'); // false
         // dd($user->wasChanged(['nama', 'username'])); // true
-        $user = UserModel::all();
+        //$user = UserModel::all();
+        // return view('user', ['data' => $user]);
+        // $user = UserModel::with('level')->get();
+        // dd($user);
+        $user =UserModel :: with('level')->get();
         return view('user', ['data' => $user]);
 
     }
--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 // class usermodel extends Model
 // {
@@ -27,4 +28,7 @@ class UserModel extends Model
     //protected $fillable = ['level_id','username','nama','password',];
 
     protected $fillable = ['level_id', 'username', 'nama', 'password'];
+    public function level(): BelongsTo{
+        return $this->belongsTo(LevelModel::class, 'level_id', 'level_id'); //relasi one to many, menggunakan foreign key level_id dan primary key level_id di table level_model
+    }
 }
